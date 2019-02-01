1765070139f792b108665c6e77215cfde9acc217
milklineep <28508187+milklineep@users.noreply.github.com>
2019-02-01 11:37:38 +0000

    Some items don't have lat/long. We can ignore them.
    This is the easiest thing to do. See https://github.com/Scarygami/location-history-json-converter/issues/20

--- location_history_json_converter.py
+++ location_history_json_converter.py
@@ -177,28 +177,29 @@ def main():
             f_out.write("  <Document>\n")
             f_out.write("    <name>Location History</name>\n")
             for item in items:
-                f_out.write("    <Placemark>\n")
-                # Order of these tags is important to make valid KML: TimeStamp, ExtendedData, then Point
-                f_out.write("      <TimeStamp><when>")
-                f_out.write(datetime.utcfromtimestamp(int(item["timestampMs"]) / 1000).strftime("%Y-%m-%dT%H:%M:%SZ"))
-                f_out.write("</when></TimeStamp>\n")
-                if "accuracy" in item or "speed" in item or "altitude" in item:
-                    f_out.write("      <ExtendedData>\n")
-                    if "accuracy" in item:
-                        f_out.write("        <Data name=\"accuracy\">\n")
-                        f_out.write("          <value>%d</value>\n" % item["accuracy"])
-                        f_out.write("        </Data>\n")
-                    if "speed" in item:
-                        f_out.write("        <Data name=\"speed\">\n")
-                        f_out.write("          <value>%d</value>\n" % item["speed"])
-                        f_out.write("        </Data>\n")
-                    if "altitude" in item:
-                        f_out.write("        <Data name=\"altitude\">\n")
-                        f_out.write("          <value>%d</value>\n" % item["altitude"])
-                        f_out.write("        </Data>\n")
-                    f_out.write("      </ExtendedData>\n")
-                f_out.write("      <Point><coordinates>%s,%s</coordinates></Point>\n" % (item["longitudeE7"] / 10000000, item["latitudeE7"] / 10000000))
-                f_out.write("    </Placemark>\n")
+                if 'longitudeE7' in item and 'latitudeE7' in item:
+                    f_out.write("    <Placemark>\n")
+                    # Order of these tags is important to make valid KML: TimeStamp, ExtendedData, then Point
+                    f_out.write("      <TimeStamp><when>")
+                    f_out.write(datetime.utcfromtimestamp(int(item["timestampMs"]) / 1000).strftime("%Y-%m-%dT%H:%M:%SZ"))
+                    f_out.write("</when></TimeStamp>\n")
+                    if "accuracy" in item or "speed" in item or "altitude" in item:
+                        f_out.write("      <ExtendedData>\n")
+                        if "accuracy" in item:
+                            f_out.write("        <Data name=\"accuracy\">\n")
+                            f_out.write("          <value>%d</value>\n" % item["accuracy"])
+                            f_out.write("        </Data>\n")
+                        if "speed" in item:
+                            f_out.write("        <Data name=\"speed\">\n")
+                            f_out.write("          <value>%d</value>\n" % item["speed"])
+                            f_out.write("        </Data>\n")
+                        if "altitude" in item:
+                            f_out.write("        <Data name=\"altitude\">\n")
+                            f_out.write("          <value>%d</value>\n" % item["altitude"])
+                            f_out.write("        </Data>\n")
+                        f_out.write("      </ExtendedData>\n")
+                    f_out.write("      <Point><coordinates>%s,%s</coordinates></Point>\n" % (item["longitudeE7"] / 10000000, item["latitudeE7"] / 10000000))
+                    f_out.write("    </Placemark>\n")
             f_out.write("  </Document>\n</kml>\n")
 
         if args.format == "gpx" or args.format == "gpxtracks":
